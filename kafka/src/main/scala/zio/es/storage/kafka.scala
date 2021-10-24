24e816f16d29b38095b73249ed671c629c2a4ca0
Landol-L <lando.loeper@gmx.net>
2021-10-24 13:06:44 +0200

    Fix kafka journal to comply with the new signature

--- kafka/src/main/scala/zio/es/storage/kafka.scala
+++ kafka/src/main/scala/zio/es/storage/kafka.scala
@@ -9,17 +9,17 @@ import zio.kafka.serde.Serde
 import zio.stream.ZStream
 
 case class KafkaStorage[E](
-  private val topic: String,
-  private val consumer: Consumer,
-  private val producer: Producer,
-  private val eventSerde: Serde[Any, E]
+    private val topic: String,
+    private val consumer: Consumer,
+    private val producer: Producer,
+    private val eventSerde: Serde[Any, E]
 ) extends EventJournal[E]:
   override def persist(id: PersistenceId)(event: E) =
     producer
       .produce(topic, id, event, KafkaStorage.persistenceIdSerde, eventSerde)
       .unit
-  
-  override def retrieve(id: PersistenceId) =
+
+  override def replay(id: PersistenceId) =
     consumer
       .plainStream(KafkaStorage.persistenceIdSerde, eventSerde)
       .filter(record => record.record.key() == id)
@@ -29,5 +29,7 @@ object KafkaStorage:
   val persistenceIdSerde: Serde[Any, PersistenceId] =
     Serde.string.inmap(PersistenceId.apply)(PersistenceId.toString)
 
-  def layer[E](topic: String)(consumer: Consumer)(producer: Producer)(eventSerde: Serde[Any, E]): ULayer[Has[EventJournal[E]]] =
+  def layer[E](topic: String)(consumer: Consumer)(producer: Producer)(
+      eventSerde: Serde[Any, E]
+  ): ULayer[Has[EventJournal[E]]] =
     ZLayer.succeed(KafkaStorage(topic, consumer, producer, eventSerde))
